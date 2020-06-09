8198a30860ae3dee73042238c0ffae0d5442b37d
Kodehustlr <kodehustlr@gmail.com>
2020-06-10 05:37:12 +0700

    username and password

---
+++ app.js
@@ -0,0 +1,68 @@
+//jshint esversion:6
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const port = 4000;
+
+const app = express();
+
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+mongoose.connect('mongodb://localhost:27017/usersDB', {useNewUrlParser: true, useUnifiedTopology: true});
+// Create a new users database
+const userSchema = {
+  email: String,
+  password: String
+}
+const User = new mongoose.model('User', userSchema);
+
+app.get('/', function(req, res) {
+  res.render('home');
+});
+
+app.get('/login', function(req, res) {
+  res.render('login');
+});
+
+app.get('/register', function(req, res) {
+  res.render('register');
+});
+
+app.post('/register', function(req, res) {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+
+  newUser.save(function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('secrets');
+    }
+  });
+});
+
+app.post('/login', function(req, res) {
+  const enteredUsername = req.body.username;
+  const enteredPassword = req.body.password;
+
+  User.findOne({email: enteredUsername}, function(err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        if (foundUser.password === enteredPassword) {
+          res.render('secrets');
+        }
+      }
+    }
+  })
+});
+
+app.listen(port, function() {
+  console.log('Server successfully started on port http://localhost:' + port);
+})
